GNOME/gnome-usage

commit d8429ed28ba168dea8342171526267c7920b2b90

add common perfomance graph view
This reduces code duplication.

Signed-off-by: Markus Göllnitz <camelcasenick@bewares.it>

src/performance-graph-view.vala
@@ -1,0 +1,50 @@
+/* performance-graph-view.vala
+ *
+ * Copyright (C) 2023 Markus Göllnitz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors: Markus Göllnitz <camelcasenick@bewares.it>
+ */
+
+public abstract class Usage.PerformanceGraphView : GraphView {
+    private Settings settings = Settings.get_default ();
+
+    construct {
+        this.range_y = 100;
+        this.offset_y = 0;
+        this.range_x = 1000 * settings.graph_timespan;
+        this.update_x_offset ();
+
+        this.add_tick_callback (this.update_x_offset);
+
+        Timeout.add (settings.graph_update_interval, () => {
+            this.update_graphs ();
+            return true;
+        });
+    }
+
+    public void add_graph (Graph graph) {
+        graph.maximal_queue_length = (int) Math.ceilf((float) settings.graph_timespan/settings.graph_update_interval) + 2;
+        base.add_graph (graph);
+    }
+
+    public bool update_x_offset () {
+        int64 timestamp = get_monotonic_time ();
+        this.offset_x = timestamp - 1000 * (settings.graph_timespan + settings.graph_update_interval);
+        return true;
+    }
+
+    protected abstract void update_graphs ();
+}

src/cpu-graph.vala
@@ -23,75 +23,48 @@
 /**
  *  Graph showing most used core
 **/
-public class Usage.CpuGraphMostUsedCore : GraphView {
-    private Settings settings = Settings.get_default ();
+public class Usage.CpuGraphMostUsedCore : PerformanceGraphView {
+    Graph max_load_graph = new Graph ();
 
     public CpuGraphMostUsedCore () {
-        Graph max_load_graph = new Graph ();
-        max_load_graph.maximal_queue_length = settings.graph_timespan/settings.graph_update_interval + 2;
-        this.add_graph (max_load_graph);
+        this.add_graph (this.max_load_graph);
+    }
 
-        this.range_y = 100;
-        this.offset_y = 0;
-        this.range_x = 1000 * settings.graph_timespan;
-        this.add_tick_callback ((widget, frame_clock) => {
-            int64 timestamp = get_monotonic_time ();
-            this.offset_x = timestamp - 1000 * (settings.graph_timespan + settings.graph_update_interval);
-            return true;
-        });
+    protected override void update_graphs () {
+        SystemMonitor monitor = SystemMonitor.get_default ();
+        int64 timestamp = get_monotonic_time ();
+        double most_used_core = monitor.x_cpu_load[0];
 
-        Timeout.add (settings.graph_update_interval, () => {
-            SystemMonitor monitor = SystemMonitor.get_default ();
-            int64 timestamp = get_monotonic_time ();
-            double most_used_core = monitor.x_cpu_load[0];
+        for (int i = 1; i < get_num_processors (); i++) {
+            if (monitor.x_cpu_load[i] > most_used_core)
+                most_used_core = monitor.x_cpu_load[i];
+        }
 
-            for (int i = 1; i < get_num_processors (); i++) {
-                if (monitor.x_cpu_load[i] > most_used_core)
-                    most_used_core = monitor.x_cpu_load[i];
-            }
-
-            GraphPoint point = GraphPoint (timestamp, most_used_core);
-            max_load_graph.push_point (point);
-
-            return true;
-        });
+        GraphPoint point = GraphPoint (timestamp, most_used_core);
+        max_load_graph.push_point (point);
     }
 }
 
 /**
  *  Graph showing all processor cores.
 **/
-public class Usage.CpuGraph : GraphView {
-    private Settings settings = Settings.get_default ();
-
+public class Usage.CpuGraph : PerformanceGraphView {
     public CpuGraph () {
         for (int i = 0; i < get_num_processors (); i++) {
             Graph graph = new Graph ();
-            graph.maximal_queue_length = settings.graph_timespan/settings.graph_update_interval + 2;
             this.add_graph (graph);
         }
 
         this.add_css_class ("big");
+    }
 
-        this.range_y = 100;
-        this.offset_y = 0;
-        this.range_x = 1000 * settings.graph_timespan;
-        this.add_tick_callback ((widget, frame_clock) => {
-            int64 timestamp = get_monotonic_time ();
-            this.offset_x = timestamp - 1000 * (settings.graph_timespan + settings.graph_update_interval);
-            return true;
-        });
+    protected override void update_graphs () {
+        SystemMonitor monitor = SystemMonitor.get_default ();
+        int64 timestamp = get_monotonic_time ();
 
-        Timeout.add (settings.graph_update_interval, () => {
-            SystemMonitor monitor = SystemMonitor.get_default ();
-            int64 timestamp = get_monotonic_time ();
-
-            for (int i = 0; i < get_num_processors (); i++) {
-                GraphPoint point = GraphPoint (timestamp, monitor.x_cpu_load[i]);
-                this.get_graph(i).push_point (point);
-            }
-
-            return true;
-        });
+        for (int i = 0; i < get_num_processors (); i++) {
+            GraphPoint point = GraphPoint (timestamp, monitor.x_cpu_load[i]);
+            this.get_graph(i).push_point (point);
+        }
     }
 }

src/memory-graph.vala
@@ -20,44 +20,30 @@
  *          Markus Göllnitz <camelcasenick@bewares.it>
  */
 
-public class Usage.MemoryGraph : GraphView {
-    private Settings settings = Settings.get_default ();
+public class Usage.MemoryGraph : PerformanceGraphView {
+    Graph mem_graph = new Graph ();
+    Graph swap_graph = new Graph ();
 
     public MemoryGraph () {
-        Graph mem_graph = new Graph ();
-        mem_graph.maximal_queue_length = settings.graph_timespan/settings.graph_update_interval + 2;
-        this.add_graph (mem_graph);
-        Graph swap_graph = new Graph ();
-        swap_graph.maximal_queue_length = settings.graph_timespan/settings.graph_update_interval + 2;
-        this.add_graph (swap_graph);
+        this.add_graph (this.mem_graph);
+        this.add_graph (this.swap_graph);
+    }
 
-        this.range_y = 100;
-        this.offset_y = 0;
-        this.range_x = 1000 * settings.graph_timespan;
-        this.add_tick_callback ((widget, frame_clock) => {
-            int64 timestamp = get_monotonic_time ();
-            this.offset_x = timestamp - 1000 * (settings.graph_timespan + settings.graph_update_interval);
-            return true;
-        });
+    protected override void update_graphs () {
+        SystemMonitor monitor = SystemMonitor.get_default ();
+        int64 timestamp = get_monotonic_time ();
 
-        Timeout.add (settings.graph_update_interval, () => {
-            SystemMonitor monitor = SystemMonitor.get_default ();
-            int64 timestamp = get_monotonic_time ();
+        double ram_usage = 0;
+        if (monitor.ram_total != 0)
+            ram_usage = (((double) monitor.ram_usage / monitor.ram_total) * 100);
 
-            double ram_usage = 0;
-            if (monitor.ram_total != 0)
-                ram_usage = (((double) monitor.ram_usage / monitor.ram_total) * 100);
+        double swap_usage = 0;
+        if (monitor.ram_total != 0)
+            swap_usage = (((double) monitor.swap_usage / monitor.swap_total) * 100);
 
-            double swap_usage = 0;
-            if (monitor.ram_total != 0)
-                swap_usage = (((double) monitor.swap_usage / monitor.swap_total) * 100);
-
-            GraphPoint mem_point = GraphPoint (timestamp, ram_usage);
-            mem_graph.push_point (mem_point);
-            GraphPoint swap_point = GraphPoint (timestamp, swap_usage);
-            swap_graph.push_point (swap_point);
-
-            return true;
-        });
+        GraphPoint mem_point = GraphPoint (timestamp, ram_usage);
+        mem_graph.push_point (mem_point);
+        GraphPoint swap_point = GraphPoint (timestamp, swap_usage);
+        swap_graph.push_point (swap_point);
     }
 }

src/meson.build
@@ -17,6 +17,7 @@ vala_sources = [
   'memory-sub-view.vala',
   'monitor.vala',
   'no-results-found-view.vala',
+  'performance-graph-view.vala',
   'performance-view.vala',
   'process-list-box.vala',
   'process-row.vala',

commit 93ce65bd222d650e2c0ce5b73c67ea9f906d0d85

performance-graph-view: make override/hiding GraphView.add_graph explicit
Fixes: d8429ed28ba168dea8342171526267c7920b2b90

Signed-off-by: Markus Göllnitz <camelcasenick@bewares.it>

src/performance-graph-view.vala
@@ -53,7 +53,7 @@ public abstract class Usage.PerformanceGraphView : GraphView {
         }
     }
 
-    public void add_graph (Graph graph) {
+    public new void add_graph (Graph graph) {
         graph.maximal_queue_length = (int) Math.ceilf((float) settings.graph_timespan/settings.graph_update_interval) + 2;
         base.add_graph (graph);
     }