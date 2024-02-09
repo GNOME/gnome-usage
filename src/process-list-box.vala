GNOME/gnome-usage

commit abad862a08c76bb30823ee35b608b4bfa46560bb

make ProcessListBoxType a struct carrying comparator, filter and widget factory
Moving parts of the different process lists are scattered
around with multiple switch statements, making it harder
to move the logic around and more complex to implement
another list type, especially when trying to rebase that.

Signed-off-by: Markus Göllnitz <camelcasenick@bewares.it>

src/process-list-box.vala
@@ -18,9 +18,12 @@
  * Authors: Petr Štětka <pstetka@redhat.com>
  */
 
-public enum Usage.ProcessListBoxType {
-    PROCESSOR,
-    MEMORY;
+public delegate bool Usage.FilterFunc<T> (T object);
+public delegate string Usage.LabelFactoryFunc<T> (T object);
+public struct Usage.ProcessListBoxType {
+    public CompareDataFunc<AppItem> comparator;
+    public Usage.FilterFunc<AppItem> filter;
+    public Usage.LabelFactoryFunc<AppItem> load_label_factory;
 }
 
 public class Usage.ProcessListBox : Adw.Bin {
@@ -66,40 +69,17 @@ public class Usage.ProcessListBox : Adw.Bin {
     private bool update () {
         model.remove_all ();
 
-        CompareDataFunc<AppItem> app_cmp = (a, b) => {
-            AppItem app_a = (AppItem) a;
-            AppItem app_b = (AppItem) b;
-
-            switch (type) {
-                default:
-                case ProcessListBoxType.PROCESSOR:
-                    return (int) ((uint64) (app_a.cpu_load < app_b.cpu_load) - (uint64) (app_a.cpu_load > app_b.cpu_load));
-                case ProcessListBoxType.MEMORY:
-                    return (int) ((uint64) (app_a.mem_usage < app_b.mem_usage) - (uint64) (app_a.mem_usage > app_b.mem_usage));
-            }
-        };
-
         var system_monitor = SystemMonitor.get_default ();
         Settings settings = Settings.get_default ();
         if (search_text == "") {
-            switch (type) {
-                default:
-                case ProcessListBoxType.PROCESSOR:
-                    foreach (unowned AppItem app in system_monitor.get_apps ()) {
-                        if (app.cpu_load > settings.app_minimum_load)
-                            model.insert_sorted (app, app_cmp);
-                    }
-                    break;
-                case ProcessListBoxType.MEMORY:
-                    foreach (unowned AppItem app in system_monitor.get_apps ())
-                        if (app.mem_usage > settings.app_minimum_memory)
-                            model.insert_sorted (app, app_cmp);
-                    break;
+            foreach (unowned AppItem app in system_monitor.get_apps ()) {
+                if (this.type.filter (app))
+                    model.insert_sorted (app, this.type.comparator);
             }
         } else {
             foreach (unowned AppItem app in system_monitor.get_apps ()) {
                 if (app.display_name.down ().contains (search_text.down ()) || app.representative_cmdline.down ().contains (search_text.down ()))
-                    model.insert_sorted (app, app_cmp);
+                    model.insert_sorted (app, this.type.comparator);
             }
         }
 

src/cpu-sub-view.vala
@@ -37,7 +37,17 @@ public class Usage.ProcessorSubView : SubView {
         cpu_graph_box.valign = Gtk.Align.START;
         cpu_graph_box.add_css_class ("card");
 
-        process_list_box = new ProcessListBox (ProcessListBoxType.PROCESSOR);
+        process_list_box = new ProcessListBox (ProcessListBoxType () {
+            comparator = (a, b) => {
+                return (int) ((uint64) (a.cpu_load < b.cpu_load) - (uint64) (a.cpu_load > b.cpu_load));
+            },
+            filter = (item) => {
+                return item.cpu_load > Settings.get_default ().app_minimum_load;
+            },
+            load_label_factory = (item) => {
+                return "%.1f %%".printf (item.cpu_load);
+            },
+        });
         process_list_box.margin_bottom = 20;
         process_list_box.margin_top = 30;
 

src/memory-sub-view.vala
@@ -30,7 +30,17 @@ public class Usage.MemorySubView : SubView {
         label.margin_top = 25;
         label.margin_bottom = 15;
 
-        process_list_box = new ProcessListBox (ProcessListBoxType.MEMORY);
+        process_list_box = new ProcessListBox (ProcessListBoxType () {
+            comparator = (a, b) => {
+                return (int) ((uint64) (a.mem_usage < b.mem_usage) - (uint64) (a.mem_usage > b.mem_usage));
+            },
+            filter = (item) => {
+                return item.mem_usage > Settings.get_default ().app_minimum_memory;
+            },
+            load_label_factory = (item) => {
+                return Utils.format_size_values (item.mem_usage);
+            },
+        });
         process_list_box.margin_bottom = 20;
         process_list_box.margin_top = 30;
 

src/process-row.vala
@@ -60,14 +60,7 @@ public class Usage.ProcessRow : Gtk.ListBoxRow {
     }
 
     private void update_load_label () {
-        switch (type) {
-            case ProcessListBoxType.PROCESSOR:
-                load_label.label = "%.1f %%".printf (app.cpu_load);
-                break;
-            case ProcessListBoxType.MEMORY:
-                load_label.label = Utils.format_size_values (app.mem_usage);
-                break;
-        }
+        load_label.label = this.type.load_label_factory (app);
     }
 
     private void update_user_tag () {

commit 529ef13691ed8a27db5061cd6ddcce21e3ccd12b

mark delegate references as unowned
Fixes: abad862a08c76bb30823ee35b608b4bfa46560bb

Signed-off-by: Markus Göllnitz <camelcasenick@bewares.it>

src/process-list-box.vala
@@ -23,9 +23,9 @@
 public delegate bool Usage.FilterFunc<T> (T object);
 public delegate Gtk.Widget Usage.WidgetFactoryFunc<T> (T object);
 public struct Usage.ProcessListBoxType {
-    public CompareDataFunc<AppItem> comparator;
-    public Usage.FilterFunc<AppItem> filter;
-    public Usage.WidgetFactoryFunc<AppItem> load_widget_factory;
+    public unowned CompareDataFunc<AppItem> comparator;
+    public unowned Usage.FilterFunc<AppItem> filter;
+    public unowned Usage.WidgetFactoryFunc<AppItem> load_widget_factory;
 }
 
 public class Usage.ProcessListBox : Adw.Bin {