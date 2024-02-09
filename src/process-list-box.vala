GNOME/gnome-usage

commit 583bfc465679f088fe3a0ed1152b4c91a313e434

performance-view: port application lists to list view
Signed-off-by: Markus Göllnitz <camelcasenick@bewares.it>

src/process-list-box.vala
@@ -1,6 +1,7 @@
 /* process-list-box.vala
  *
  * Copyright (C) 2017 Red Hat, Inc.
+ * Copyright (C) 2023-2024 Markus Göllnitz
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,6 +17,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * Authors: Petr Štětka <pstetka@redhat.com>
+ *          Markus Göllnitz <camelcasenick@bewares.it>
  */
 
 public delegate bool Usage.FilterFunc<T> (T object);
@@ -27,7 +29,7 @@ public struct Usage.ProcessListBoxType {
 }
 
 public class Usage.ProcessListBox : Adw.Bin {
-    public Gtk.ListBox list_box { get; private set; default = new Gtk.ListBox (); }
+    public Gtk.ListView list_view { get; private set; }
 
     public bool empty { get; set; default = true; }
     public string search_text { get; set; default = ""; }
@@ -36,19 +38,27 @@ public class Usage.ProcessListBox : Adw.Bin {
     private ProcessListBoxType type;
 
     public ProcessListBox (ProcessListBoxType type) {
-        this.set_child (list_box);
-
-        list_box.set_selection_mode (Gtk.SelectionMode.NONE);
-        list_box.add_css_class ("boxed-list");
-
         this.type = type;
-        model = new ListStore (typeof (AppItem));
-        list_box.bind_model (model, on_row_created);
+        this.model = new ListStore (typeof (ProcessRowItem));
+        typeof (Usage.ProcessUserTag).ensure ();
+        Gtk.BuilderListItemFactory factory = new Gtk.BuilderListItemFactory.from_resource (null, "/org/gnome/Usage/ui/process-row.ui");
+        this.list_view = new Gtk.ListView (new Gtk.NoSelection (model), factory);
 
-        list_box.row_activated.connect ((row) => {
-            var process_row = (ProcessRow) row;
-            process_row.activate ();
+        this.list_view.add_css_class ("card");
+        this.list_view.show_separators = true;
+        this.list_view.single_click_activate = true;
+
+        this.list_view.activate.connect ((list_view, position) => {
+            AppItem app = (list_view.get_model ().get_item (position) as ProcessRowItem).app;
+
+            if (app.is_killable ()) {
+                var dialog = new QuitProcessDialog (app);
+                dialog.set_transient_for ((Gtk.Window) this.get_root ());
+                dialog.present ();
+            }
         });
+
+        this.set_child (list_view);
 
         this.notify["search-text"].connect ((sender, property) => {
             update ();
@@ -63,31 +73,31 @@ public class Usage.ProcessListBox : Adw.Bin {
         var settings = Settings.get_default ();
         Timeout.add (settings.list_update_interval_UI, update);
 
-        bind_property ("empty", this, "visible", BindingFlags.INVERT_BOOLEAN);
+        this.bind_property ("empty", this, "visible", BindingFlags.INVERT_BOOLEAN | BindingFlags.SYNC_CREATE);
     }
 
     private bool update () {
         model.remove_all ();
+
+        CompareDataFunc<ProcessRowItem> app_cmp = (a, b) => {
+            return this.type.comparator (((ProcessRowItem) a).app, ((ProcessRowItem) b).app);
+        };
 
         var system_monitor = SystemMonitor.get_default ();
         Settings settings = Settings.get_default ();
         if (search_text == "") {
             foreach (unowned AppItem app in system_monitor.get_apps ()) {
                 if (this.type.filter (app))
-                    model.insert_sorted (app, this.type.comparator);
+                    model.insert_sorted (new ProcessRowItem (app, type), app_cmp);
             }
         } else {
             foreach (unowned AppItem app in system_monitor.get_apps ()) {
                 if (app.display_name.down ().contains (search_text.down ()) || app.representative_cmdline.down ().contains (search_text.down ()))
-                    model.insert_sorted (app, this.type.comparator);
+                    model.insert_sorted (new ProcessRowItem (app, type), app_cmp);
             }
         }
 
         empty = (model.get_n_items () == 0);
         return true;
-    }
-
-    private Gtk.Widget on_row_created (Object item) {
-        return new ProcessRow ((AppItem) item, type);
     }
 }

data/ui/process-row.ui
@@ -1,8 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <interface>
   <requires lib="gtk" version="4.0"/>
-  <template class="UsageProcessRow" parent="GtkListBoxRow">
-    <property name="hexpand">True</property>
+  <template class="GtkListItem">
     <property name="child">
       <object class="GtkBox">
         <property name="orientation">vertical</property>
@@ -19,7 +18,7 @@
                 <binding name="gicon">
                   <lookup name="icon" type="UsageAppItem">
                     <lookup name="app" type="UsageProcessRowItem">
-                      <lookup name="item" type="UsageProcessRow"/>
+                      <lookup name="item" type="GtkListItem"/>
                     </lookup>
                   </lookup>
                 </binding>
@@ -34,7 +33,7 @@
                 <binding name="label">
                   <lookup name="display_name" type="UsageAppItem">
                     <lookup name="app" type="UsageProcessRowItem">
-                      <lookup name="item" type="UsageProcessRow"/>
+                      <lookup name="item" type="GtkListItem"/>
                     </lookup>
                   </lookup>
                 </binding>
@@ -47,7 +46,7 @@
                 <binding name="visible">
                   <lookup name="gamemode" type="UsageAppItem">
                     <lookup name="app" type="UsageProcessRowItem">
-                      <lookup name="item" type="UsageProcessRow"/>
+                      <lookup name="item" type="GtkListItem"/>
                     </lookup>
                   </lookup>
                 </binding>
@@ -57,17 +56,17 @@
               <object class="UsageProcessUserTag" id="user_tag">
                 <binding name="visible">
                   <lookup name="not_current_user" type="UsageProcessRowItem">
-                    <lookup name="item" type="UsageProcessRow"/>
+                    <lookup name="item" type="GtkListItem"/>
                   </lookup>
                 </binding>
                 <binding name="label">
                   <lookup name="user" type="UsageProcessRowItem">
-                    <lookup name="item" type="UsageProcessRow"/>
+                    <lookup name="item" type="GtkListItem"/>
                   </lookup>
                 </binding>
                 <binding name="user_type">
                   <lookup name="user_type" type="UsageProcessRowItem">
-                    <lookup name="item" type="UsageProcessRow"/>
+                    <lookup name="item" type="GtkListItem"/>
                   </lookup>
                 </binding>
               </object>
@@ -78,7 +77,7 @@
                 <property name="max_width_chars">30</property>
                 <binding name="label">
                   <lookup name="load" type="UsageProcessRowItem">
-                    <lookup name="item" type="UsageProcessRow"/>
+                    <lookup name="item" type="GtkListItem"/>
                   </lookup>
                 </binding>
               </object>

src/meson.build
@@ -20,7 +20,7 @@ vala_sources = [
   'performance-graph-view.vala',
   'performance-view.vala',
   'process-list-box.vala',
-  'process-row.vala',
+  'process-row-item.vala',
   'process-user-tag.vala',
   'process.vala',
   'quit-process-dialog.vala',

src/process-row-item.vala
@@ -20,30 +20,6 @@
  *          Markus Göllnitz <camelcasenick@bewares.it>
  */
 
-[GtkTemplate (ui = "/org/gnome/Usage/ui/process-row.ui")]
-public class Usage.ProcessRow : Gtk.ListBoxRow {
-    public AppItem app { get; private set; }
-    public ProcessRowItem item { get; private set; }
-
-    [GtkChild]
-    private unowned Usage.ProcessUserTag user_tag;
-
-    public ProcessRow (AppItem app, ProcessListBoxType type) {
-        this.app = app;
-        this.item = new ProcessRowItem (app, type);
-    }
-
-    public new void activate () {
-        var settings = Settings.get_default ();
-        if (app.representative_cmdline in settings.get_strv ("unkillable-processes"))
-            return;
-
-        var dialog = new QuitProcessDialog (app);
-        dialog.set_transient_for ((Gtk.Window) this.get_root ());
-        dialog.present ();
-    }
-}
-
 public class Usage.ProcessRowItem : Object {
     private const string CSS_TAG_USER = "tag-user";
     private const string CSS_TAG_ROOT = "tag-root";

commit 4bb22859cbb3c797648133a67ad20800a2886ed8

process-list-box: use non-nullable casting
Fixes: 583bfc465679f088fe3a0ed1152b4c91a313e434

Signed-off-by: Markus Göllnitz <camelcasenick@bewares.it>

src/process-list-box.vala
@@ -70,7 +70,7 @@ public class Usage.ProcessListBox : Adw.Bin {
         this.list_view.single_click_activate = true;
 
         this.list_view.activate.connect ((list_view, position) => {
-            AppItem app = (list_view.get_model ().get_item (position) as ProcessRowItem).app;
+            AppItem app = ((ProcessRowItem) list_view.get_model ().get_item (position)).app;
 
             if (app.is_killable ()) {
                 var dialog = new QuitProcessDialog (app);